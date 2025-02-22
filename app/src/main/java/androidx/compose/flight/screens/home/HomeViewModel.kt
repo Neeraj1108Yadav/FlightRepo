c3fb64db379d9623371190029b0c33f15b198f68
Neeraj <neeraj@avantari.org>
2025-02-22 18:32:22 +0530

    set error using animation visibility

--- app/src/main/java/androidx/compose/flight/screens/home/HomeViewModel.kt
+++ app/src/main/java/androidx/compose/flight/screens/home/HomeViewModel.kt
@@ -4,6 +4,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 val MAX_PEOPLE = 4
 
@@ -12,8 +15,17 @@ class HomeViewModel : ViewModel() {
     var people by mutableStateOf(1)
         private set
 
+    var isValidPeopleCount by mutableStateOf(false)
+        private set
+
+
     fun addPeople(){
         people = (people % (MAX_PEOPLE + 1)) + 1
+        updateInputUserState()
+    }
+
+    private fun updateInputUserState(){
+        isValidPeopleCount = people > MAX_PEOPLE
     }
 
 }
--- app/src/main/java/androidx/compose/flight/screens/home/PeopleUserInput.kt
+++ app/src/main/java/androidx/compose/flight/screens/home/PeopleUserInput.kt
@@ -1,9 +1,17 @@
 package androidx.compose.flight.screens.home
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.flight.components.UserInput
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.flight.R
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 
 @Composable
 fun PeopleUserInput(
@@ -12,6 +20,13 @@ fun PeopleUserInput(
 ){
     Column {
         val people = viewModel.people
+        val isValidPeople = viewModel.isValidPeopleCount
+        val transitionState = remember { MutableTransitionState(false) }
+
+        LaunchedEffect(isValidPeople) {
+            transitionState.targetState = isValidPeople
+        }
+
         UserInput(
             text = if(people == 1) "$people Adult$titleSuffix" else "$people Adults$titleSuffix",
             vectorImageId = R.drawable.ic_person,
@@ -19,5 +34,15 @@ fun PeopleUserInput(
                 viewModel.addPeople()
             }
         )
+        AnimatedVisibility(
+            visibleState = transitionState,
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) {
+            Text(
+                text = "Error: We don't support more than $MAX_PEOPLE people",
+                style = MaterialTheme.typography.labelLarge.copy(color = MaterialTheme.colorScheme.errorContainer)
+            )
+        }
     }
 }
